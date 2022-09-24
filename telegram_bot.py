875f2d69ca180b891a4d88c76c86f74337100cf7
Flash-kir <flash-kir@yandex.ru>
2022-09-24 14:52:24 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 import telegram
 
+chat_id = '@Cosmos_pictures'
 bot = telegram.Bot(token=os.environ.get('TELEGRAM_TOKEN'))
 #print(bot.get_me())
-bot.send_message(chat_id='@Cosmos_pictures', text='Hello, my subscribers! Glad to see you in my channel!')
+bot.send_message(chat_id=chat_id, text='Image from spacex launch')
+bot.send_document(chat_id=chat_id, document=open('images/50291306061_2f9e350a85_o.jpg', 'rb'))
